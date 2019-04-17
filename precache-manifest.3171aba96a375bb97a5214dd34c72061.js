c58c302594c00da1c264a167826530ff18199ea8
Sungho,Yun <itlockit@gmail.com>
2019-04-18 02:42:40 +0900

    Updates

---
+++ precache-manifest.3171aba96a375bb97a5214dd34c72061.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "618f310c1c3d9f0ec4bf0ffeb1977463",
+    "url": "/movie_app/static/media/KoPubWorldLight.618f310c.otf"
+  },
+  {
+    "revision": "31732071d334330804beba8da74e8296",
+    "url": "/movie_app/static/media/KoPubWorldBold.31732071.otf"
+  },
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "fd48dd79cfb1f199abb2",
+    "url": "/movie_app/static/js/main.aeaee236.chunk.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/movie_app/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "fd48dd79cfb1f199abb2",
+    "url": "/movie_app/static/css/main.0508974a.chunk.css"
+  },
+  {
+    "revision": "3a7df2dd65456e896c93ea378bc7f4bf",
+    "url": "/movie_app/index.html"
+  }
+];
