d813a6b0913393dbac9bd09a42e3394de57509cc
Sungho,Yun <itlockit@gmail.com>
2019-04-17 21:37:17 +0900

    데이터 구조화 작업
    api로부터 데이터 불러와서 화면에 띄운다.

--- src/App.js
+++ src/App.js
@@ -2,45 +2,47 @@ import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie";
 
-const movieTitles = ["Matrix", "Full Metal Jacket", "Oldboy", "Star Wars"];
-const movieImages = [
-  "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/41TvQPuP-XL._SY450_.jpg",
-  "https://ih1.redbubble.net/image.224519982.2775/flat,550x550,075,f.u2.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
-];
-
-const movies = [
-  {
-    title: "Matrix",
-    poster:
-      "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg"
-  },
-  {
-    title: "Full Meta Jacket",
-    poster:
-      "https://images-na.ssl-images-amazon.com/images/I/41TvQPuP-XL._SY450_.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster:
-      "https://ih1.redbubble.net/image.224519982.2775/flat,550x550,075,f.u2.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster:
-      "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
-  }
-];
 class App extends Component {
+  state = {};
+  componentDidMount() {
+    this._getMovies();
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return (
+        <Movie
+          title={movie.title}
+          poster={movie.medium_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
+    });
+    return movies;
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch(
+      "https://yts.am/api/v2/list_movies.json?sort_by=download_count"
+    )
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  };
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {movies.map((movie, index) => {
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index} />
-          );
-        })}
+      <div className={movies ? "App" : "App--loading"}>
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,27 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-class Movie extends Component {
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster} />
-        <h1>{this.props.title}</h1>
+function Movie({ title, poster, genres, synopsis }) {
+  return (
+    <div className="Movie">
+      <div className="Movie_Columns">
+        <MoviePoster poster={poster} alt={title} />
       </div>
-    );
-  }
+      <div className="Movie_Columns">
+        <h1>{title}</h1>
+        <div className="Movie__Genres" />
+        {genres.map((genre, index) => (
+          <MovieGenre genre={genre} key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
+function MoviePoster({ poster, alt }) {
+  return <img className="Movie__Poster" src={poster} alt={alt} title={alt} />;
+}
+
+function MovieGenre({ genre }) {
+  return <span className="Movie__Genre">{genre}</span>;
+}
+
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired
 };
-class MoviePoster extends Component {
-  render() {
-    return <img src={this.props.poster} />;
-  }
-}
+
 MoviePoster.propTypes = {
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
 };
+
+MovieGenre.prototypes = {
+  genre: PropTypes.string.isRequired
+};
+
 export default Movie;
